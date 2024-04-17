bb994be969c7df93740573488c2f2e2f11408590
quinonesnn <quinonesnn@gmail.com>
2024-04-17 13:05:39 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent 
+  ],
+})
+export class AppModule { }
